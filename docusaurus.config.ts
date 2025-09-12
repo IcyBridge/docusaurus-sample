8e4da08a9edcee2e2cda9b0ebb1bf5f41ce08695
Timo Lassila <timo.lassila@savonia.fi>
2025-09-12 13:59:56 +0300

    baseURL of documentation fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://icybridge.github.io/docusaurus-sample/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-sample/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
