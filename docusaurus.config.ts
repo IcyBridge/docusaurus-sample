ad08f0a69058e68966d78799eaadedf338f10280
Timo Lassila <timo.lassila@savonia.fi>
2025-09-12 15:12:13 +0300

    Search functionality add to the docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
+  plugins: [require.resolve('docusaurus-lunr-search')],
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -44,8 +45,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
